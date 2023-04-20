10efdc6b72a339e49153c9414d2683e9bfec0e82
m.tygyshev <m.tygyshev@univertv.ru>
2023-04-20 21:50:30 +0300

    main: eslint config; auth provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
+import App from './App';
 
 const container = document.getElementById('app-root')!;
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Auth0Provider
+    domain=""
+    clientId=""
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>,
+);
